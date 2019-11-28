070649da2adb0bf1ca2ba8db477daee891fcc58f
abhishek <abhishek7gg7@gmail.com>
2019-11-28 18:51:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var app = require('express')();
 app.listen(process.env.PORT);
 
 app.get("/*",function(req,res) {
-res.sendFile(__dirname+'/public'+req.url , { root : __dirname});
+res.sendFile(__dirname+'/public'+req.url);
 });
